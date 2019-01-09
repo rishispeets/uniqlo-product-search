143d135b5f70320d3a87dd4334268fc5dd268873
rishispeets <hello@rishispeets.com>
2019-01-09 19:52:56 +0100

    Add rudimentary parser

--- handler.js
+++ handler.js
@@ -1,10 +1,12 @@
 "use strict";
 const axios = require("axios");
+const htmlparser = require("htmlparser2");
+
+const URL = "https://www.uniqlo.com/eu/en_NL/men/outerwear/coats-jackets";
+const SEARCH_TERMS = ["wool", "chesterfield", "coat"];
 
 async function checkStock(event, context, cb) {
-  const html = await getHtml(
-    "https://www.uniqlo.com/eu/en_NL/men/outerwear/coats-jackets"
-  );
+  const html = await getHtml(URL);
   const parsedHtml = parseHtml(html);
 
   return {
@@ -19,13 +21,34 @@ async function checkStock(event, context, cb) {
 async function getHtml(url) {
   try {
     const response = await axios.get(url);
-    return response;
+    return response.data;
   } catch (error) {
     console.error(error);
   }
 }
 
-function parseHtml() {}
+function parseHtml(html) {
+  const parser = createParser();
+  parser.write(html);
+}
+
+function createParser() {
+  return new htmlparser.Parser({
+    onopentag: (name, attribs) => {
+      if (name === "a" && attribs.title) {
+        console.log(`Example of a title: ${attribs.title}`);
+      }
+    }
+  });
+}
+
+function collectAndFilterTitles() {}
+function isValidTitle(title) {
+  return isAllCaps(title) && !isSingleWord(title);
+}
+function isAllCaps() {}
+function isSingleWord() {}
+function isATag() {}
 
 module.exports = {
   checkStock,
--- handler.test.js
+++ handler.test.js
@@ -5,15 +5,32 @@ const htmlparser = require("htmlparser2");
 jest.mock("axios");
 
 describe("getHtml", () => {
-  test("it should retrieve html", async () => {
-    const url = "https://www.uniqlo.com/eu/en_NL/men/outerwear/coats-jackets";
-    const response = "<html><body><p>hello</p></body></html>";
+  const URL = "https://www.uniqlo.com/eu/en_NL/men/outerwear/coats-jackets";
+  const mockAxiosImplAndGetResult = async fakeResponse => {
+    axios.get.mockImplementation(() => Promise.resolve(fakeResponse));
+    return await handler.getHtml(URL);
+  };
 
-    axios.get.mockImplementation(() => Promise.resolve(response));
-    const result = await handler.getHtml(url);
+  test("it should retrieve html", async () => {
+    const fakeResponse = "<html><body><p>hello</p></body></html>";
+
+    const result = await mockAxiosImplAndGetResult(fakeResponse);
 
     expect(typeof result).toBe("string");
     expect(probablyHasHtmlElements(result)).toBe(true);
+  });
+
+  test("it should log an error when thrown by the API", async () => {
+    const fakeResponse = JSON.stringify({
+      statusCode: 404,
+      body: "Just an error"
+    });
+
+    const result = await mockAxiosImplAndGetResult(fakeResponse);
+
+    expect(typeof result).toBe("string");
+    expect(probablyHasHtmlElements(result)).toBe(false);
+    // TODO: Add another test here without mocking the console.
   });
 });
 
