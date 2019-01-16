e5e0cc4297b46add6a5e9e8c8d8ef8b34739e984
rishispeets <hello@rishispeets.com>
2019-01-16 21:16:32 +0100

    clean up handler

--- handler.js
+++ handler.js
@@ -1,18 +1,14 @@
 const searchStock = require("./src/index");
 
-const URL = "https://www.uniqlo.com/eu/en_NL/men/outerwear/coats-jackets";
-const SEARCH_TERMS = "wool CHESTERFIELD coat";
-
 module.exports = { checkstock };
 
-async function checkstock(event) {
-  const searchMatches = await searchStock(URL, SEARCH_TERMS);
-  return respondWithMatches(searchMatches);
+async function checkstock({ url, searchTerms }) {
+  if (!isUniqloUrl(URL))
+    return new Error("Please pass a Uniqlo url with product listings");
+
+  return searchStock(url, searchTerms);
 }
 
-function respondWithMatches(matches) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(matches)
-  };
+function isUniqloUrl(url) {
+  return url.includes("uniqlo.com");
 }
